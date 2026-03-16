394f373d90de43a3367ed8a12fb83aeb291429b4
Shini <ginoy26@gmail.com>
2026-03-16 16:05:26 +0800

    empty website fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  },
+  base: '/',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
